97c3f6c3238fb856374eddd8225aff8c879c1d91
shouya0331 <shouya0331@ezweb.ne.jp>
2020-04-20 16:29:02 +0900

    メッセージ画面の調整

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,7 @@ class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
   has_one_attached :image
+  # validates :content, presence: true, unless: :image_upload
 
   def image_upload
     return self.image.variant(resize: '500x500')

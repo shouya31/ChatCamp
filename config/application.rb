28d5229d7bbd01048210c2a222ad20d2642bd381
shouya0331 <shouya0331@ezweb.ne.jp>
2020-04-21 12:14:38 +0900

    フォームのボタンの配置調整

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,9 @@ module ChatCamp
     config.load_defaults 6.0
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
+    config.action_view.field_error_proc = Proc.new do |html_tag, instance|
+      %Q(#{html_tag}).html_safe
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers

9b0ab5add3a1b451304b7d65bfc15ec67302652e
shouya0331 <shouya0331@ezweb.ne.jp>
2020-06-03 14:51:39 +0900

    メッセージのモデルテスト

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,31 @@
+
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      user = FactoryBot.create(:user)
+      room = FactoryBot.create(:room)
+      @message = FactoryBot.build(:message, user_id: user.id, room_id: room.id)
+      @message.image =  fixture_file_upload(Rails.root.join('public/images/test_image.png'))
+    end
+
+    it 'contentとimageが存在していれば保存できること' do
+      expect(@message).to be_valid
+    end
+    it 'contentが存在していれば保存できること' do
+      @message.image = nil
+      expect(@message).to be_valid
+    end
+    it 'imageが存在していれば保存できること' do
+      @message.content = nil
+      expect(@message).to be_valid
+    end
+    it 'contentとimageが空では保存できないこと' do
+      @message.content = nil
+      @message.image = nil
+      @message.valid?
+      expect(@message.errors[:content]).to include("can't be blank")
+    end
+  end
 end
